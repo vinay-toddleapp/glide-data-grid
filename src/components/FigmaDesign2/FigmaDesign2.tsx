0ecd63cad0c7b09c37903467cab2386e1bb9cfd7
vinay-toddleapp <vinay.pratapsingh@toddleapp.com>
2024-08-05 09:18:11 +0530

    feat: updated draw method for student name in figma design 2

--- src/components/FigmaDesign2/FigmaDesign2.tsx
+++ src/components/FigmaDesign2/FigmaDesign2.tsx
@@ -36,7 +36,7 @@ const FigmaDesign2 = () => {
           kind: GridCellKind.Custom,
           allowOverlay: false,
           readonly: true,
-          data: 0,
+          data: data,
           copyData: data.toString(),
         };
       } else if (col === 1) {
@@ -71,6 +71,106 @@ const FigmaDesign2 = () => {
       return cell.kind === GridCellKind.Custom;
     },
     draw: (args: DrawArgs<ICustomCell>) => {
+      const { cell, ctx, rect } = args;
+      const { x, y, width, height } = rect;
+      const {
+        prefixBgColor,
+        prefixText,
+        prefixTextColor,
+        subTitle,
+        suffixBgColor,
+        suffixText,
+        suffixTextColor,
+        title,
+      } = cell.data;
+
+      const gap = 8;
+      let titleX = x + gap;
+      let titleY = y + height / 2;
+
+      // draw circular prefix
+      const prefixRadius = 16;
+      const prefixCenterX = x + prefixRadius + gap;
+      const prefixCenterY = y + height / 2;
+
+      ctx.beginPath();
+      ctx.arc(prefixCenterX, prefixCenterY, prefixRadius, 0, Math.PI * 2);
+      ctx.fillStyle = prefixBgColor;
+      ctx.fill();
+      ctx.closePath();
+
+      ctx.fillStyle = prefixTextColor;
+      ctx.font = "600 12px Arial";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(prefixText, prefixCenterX, prefixCenterY);
+
+      titleX = prefixCenterX + prefixRadius + gap;
+
+      // Adjust title position
+      titleY = y + height / 2 - 8;
+
+      // Draw title
+      ctx.fillStyle = "black";
+      ctx.font = "16px Arial";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "middle";
+      ctx.fillText(title, titleX, titleY);
+
+      // Draw subtitle
+      ctx.fillStyle = "#717171";
+      ctx.font = "500 12px Arial";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "middle";
+      ctx.fillText(subTitle, titleX, titleY + 16);
+
+      // draw rectangular suffix with border radius
+      const suffixWidth = 21;
+      const suffixHeight = 24;
+      const suffixX = x + width - suffixWidth - gap;
+      const suffixY = y + (height - suffixHeight) / 2;
+      const borderRadius = 3;
+
+      ctx.beginPath();
+      ctx.moveTo(suffixX + borderRadius, suffixY);
+      ctx.lineTo(suffixX + suffixWidth - borderRadius, suffixY);
+      ctx.quadraticCurveTo(
+        suffixX + suffixWidth,
+        suffixY,
+        suffixX + suffixWidth,
+        suffixY + borderRadius
+      );
+      ctx.lineTo(suffixX + suffixWidth, suffixY + suffixHeight - borderRadius);
+      ctx.quadraticCurveTo(
+        suffixX + suffixWidth,
+        suffixY + suffixHeight,
+        suffixX + suffixWidth - borderRadius,
+        suffixY + suffixHeight
+      );
+      ctx.lineTo(suffixX + borderRadius, suffixY + suffixHeight);
+      ctx.quadraticCurveTo(
+        suffixX,
+        suffixY + suffixHeight,
+        suffixX,
+        suffixY + suffixHeight - borderRadius
+      );
+      ctx.lineTo(suffixX, suffixY + borderRadius);
+      ctx.quadraticCurveTo(suffixX, suffixY, suffixX + borderRadius, suffixY);
+      ctx.closePath();
+
+      ctx.fillStyle = suffixBgColor;
+      ctx.fill();
+
+      ctx.fillStyle = suffixTextColor;
+      ctx.font = "500 12px Arial";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(
+        suffixText,
+        suffixX + suffixWidth / 2,
+        suffixY + suffixHeight / 2
+      );
+
       return true;
     },
   };
@@ -84,6 +184,7 @@ const FigmaDesign2 = () => {
       columns={figmaDesignColumn2}
       rows={tableData.length}
       customRenderers={[renderer]}
+      rowHeight={44}
     />
   );
 };
